afbd3e9e02315e9f091477b3ce71c4c4451cff8c
SusanaAlve <sf.alves@campus.fct.unl.pt>
2025-07-18 00:03:55 +0100

    fix:cenas

--- chiron_control_center.py
+++ chiron_control_center.py
@@ -2534,6 +2534,8 @@ def page_running_simulations():
                         dm_stage, current_decision_index = 4, rel_idx + 1
                     elif flat_questions ==  b3 + b4 + b5:
                         dm_stage, current_decision_index = 6, rel_idx + 1
+                    elif flat_questions == b6:
+                        dm_stage, current_decision_index = 8, rel_idx + 1
                     else:
                         dm_stage, current_decision_index = 12, rel_idx + 1
 
