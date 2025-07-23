b5927d0809d6bc3652bd2c255364dcfa15e21be3
SusanaAlve <sf.alves@campus.fct.unl.pt>
2025-07-23 09:56:24 +0100

    fix:ff

--- chiron_control_center.py
+++ chiron_control_center.py
@@ -2984,13 +2984,13 @@ def page_running_simulations():
                 st.session_state.current_decision_index = current_decision_index
                 st.session_state.dm_stage               = dm_stage
                 # st.write("🔍 debugging all_questions:", st.session_state.all_questions[:5])
-                idx = st.session_state.current_decision_index
-                st.write("🔍 debug current_decision_index:", idx)
-                if idx is not None:
-                     q = st.session_state.all_questions[idx]
-                     st.write("🔍 debug question:", q["inject"])
-                else:
-                    st.write("🔍 currently at an inject, no question index to show")
+                # idx = st.session_state.current_decision_index
+                # st.write("🔍 debug current_decision_index:", idx)
+                # if idx is not None:
+                #      q = st.session_state.all_questions[idx]
+                #      st.write("🔍 debug question:", q["inject"])
+                # else:
+                #     st.write("🔍 currently at an inject, no question index to show")
                 
                 ans12 = get_role_decision_answer("Decision 12", st.session_state.dm_role)
                 ans22 = get_role_decision_answer("Decision 22", st.session_state.dm_role)
