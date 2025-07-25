b90cb58e20607be7aec01d42f7f4f85346a1549a
SusanaAlve <sf.alves@campus.fct.unl.pt>
2025-07-25 19:38:57 +0100

    fix:ll

--- questionnaire1.py
+++ questionnaire1.py
@@ -3499,11 +3499,6 @@ def show_initial_situation():
     part_id = st.session_state.participant_id
     st.subheader("🚀 Initial Situation")  
     st.write("For the 17th day of the mission, an EVA is planned to install a component on a radiation collection device. **EVA Crew:** EVA1 (Mission Specialist) & EVA2 (Flight Engineer) **Inside Crew:** Commander (CMO) & FE-3(IV1) (IV Crew Member).")
-    if st.button("▶️ Start Simulation"):
-        _persist_inject_done(sim_id, part_id, "Initial Situation")
-        st.session_state.answers["Initial Situation"] = "DONE"
-        st.session_state.dm_stage = 1
-        st.rerun()
 
 
 def inject3():
